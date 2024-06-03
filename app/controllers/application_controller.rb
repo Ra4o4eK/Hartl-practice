58fd55dabdd40f214e24bc87b8d5f76aa1ff00d0
Ilya Akinshin <ra4o4ek777@gmail.com>
2024-06-03 15:04:31 +0300

    Adding users model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render plain: "HELO"
+    render plain: "¡Hola, mundo!"
+  end
+
+  def goodbye
+    render plain: "BYE"
   end
 end
